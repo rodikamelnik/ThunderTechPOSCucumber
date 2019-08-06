34b7250576131cd8cf7e74f5a0d92044aeb0c476
Rodica <melnik.rodika@gmail.com>
2019-08-06 16:34:08 -0500

    my last version

--- src/test/java/pages/ProductsPage.java
+++ src/test/java/pages/ProductsPage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -50,12 +51,23 @@ public class ProductsPage {
     @FindBy(xpath = "//div[@name='list_price']")
     public WebElement SalesPrice;
 
+    //This button arranges all products in a list
     @FindBy(xpath = "//button[@aria-label='list']")
     public WebElement listButton;
 
+    // Actin Button, pop ups after Manager selects one product from list
     @FindBy(xpath = "(//button[@class='o_dropdown_toggler_btn btn btn-sm dropdown-toggle'])[2]")
     public WebElement ActionButton;
 
     @FindBy(xpath = "(//select[@class='o_input o_field_widget o_required_modifier'])[1]")
     public WebElement productType;
+
+    //WebElement OK button for both pop up windows, confirmatin window and error window
+    @FindBy(xpath = "//button[@class='btn btn-sm btn-primary']")
+    public WebElement okButton;
+
+
+    //WebElement Delete option from Action Button
+    @FindBy(xpath = "(//a[@data-section='other'])[4]")
+    public WebElement deleteOption;
 }
--- src/test/java/steps/EditProduct_steps.java
+++ src/test/java/steps/EditProduct_steps.java
@@ -21,12 +21,13 @@ public class EditProduct_steps {
     ProductsPage products = new ProductsPage();
     Actions actions = new Actions(Driver.getDriver());
 
-
+    // Manager is is logging in Odoo page with manager credentials
     @When("Manager is on Odoo page")
     public void manager_is_on_Odoo_page()throws InterruptedException {
         login.loginPage();
         login.login("POSManager7");
     }
+
 
     @When("Manager clicks on Point of Sale functionality")
     public void manager_clicks_on_Point_of_Sale_functionality()throws InterruptedException {
@@ -35,6 +36,7 @@ public class EditProduct_steps {
 
     }
 
+    // verifing if manager is on Point of Sales page, we check if current URL contains expected URL
     @Then("Manager is on Point of Sale page")
     public void manager_is_on_Point_of_Sale_page(){
         Assert.assertTrue( Driver.getDriver().getCurrentUrl().contains("http://54.148.96.210/web?#view_type=kanban&model=pos.config&menu_id=500&action=677"));
@@ -47,6 +49,7 @@ public class EditProduct_steps {
         productsModule.click();
     }
 
+    // Manager should see all products, using for each loop, cheking if each product is displayed
     @Then("Manager should see all products")
     public void manager_should_see_all_products() {
         List<WebElement> allProducts = Driver.getDriver().findElements(By.className("o_kanban_view o_kanban_ungrouped"));
@@ -54,12 +57,14 @@ public class EditProduct_steps {
             Assert.assertTrue(element.isDisplayed());
         }
     }
-
+    // manager clicks on a random product, in this case first product
     @When("Manager clicks on any product")
     public void manager_clicks_on_any_product() {
         products.product.click();
     }
 
+    // after clicking on first product, window with info will pop up and manager is able to edit information, on highlited tabs
+    // we clear previous product name and change the input, and change product type
     @Then("Window with info will pop up and user is able to edit")
     public void window_with_info_will_pop_up_and_user_is_able_to_edit()throws InterruptedException {
         products.edit.click();
@@ -77,7 +82,8 @@ public class EditProduct_steps {
     }
 
 
-
+    // first manager is clicking on listButton, wich arranges all products in a list, and then selects one product, Action button
+    // should pop up on top
     @When("Manager selects one product and Action button pop up")
     public void manager_selects_one_product_and_Action_button_pop_up()throws InterruptedException {
        products.listButton.click();
@@ -87,43 +93,44 @@ public class EditProduct_steps {
        Assert.assertTrue(products.ActionButton.isDisplayed());
 
     }
-
+    // Action button has 4 options, manager should select and click on delete option
     @When("Manager select delete option")
     public void manager_select_delete_option() throws InterruptedException{
         products.ActionButton.click();
         Thread.sleep(2000);
-        WebElement deleteOption = Driver.getDriver().findElement(By.xpath("(//a[@data-section='other'])[4]"));
-        Thread.sleep(2000);
-        deleteOption.click();
+        products.deleteOption.click();
 
     }
-
+    // a window will pop up with confirmation message
     @Then("Confirmation window pop up")
     public void confirmation_window_pop_up() {
         WebElement confirmationWindow = Driver.getDriver().findElement(By.xpath("//div[@class='modal-content']"));
         Assert.assertTrue(confirmationWindow.isDisplayed());
 
-
     }
 
     @When("Manager clicks Ok")
     public void manager_clicks_Ok()throws InterruptedException {
-        WebElement okButton = Driver.getDriver().findElement(By.xpath("//button[@class='btn btn-sm btn-primary']"));
         Thread.sleep(2000);
-        okButton.click();
+        products.okButton.click();
 
     }
 
+    // after clicking ok, another window pop ups with error message, and manager again clicks ok
     @When("Error message pop up and manager clicks Ok")
     public void error_message_pop_up_and_manager_clicks_Ok() throws InterruptedException{
         WebElement errorWindow = Driver.getDriver().findElement(By.xpath("//div[@class='modal-content']"));
-        WebElement okErrorButton = Driver.getDriver().findElement(By.xpath("(//button[@type='button'])[20]"));
         Thread.sleep(2000);
-        okErrorButton.click();
+        products.okButton.click();
     }
 
+    // Manager should be able to see the list of products from begining
     @Then("List of POS appear")
     public void list_of_POS_appear() {
+        List <WebElement> allProducts = Driver.getDriver().findElements(By.xpath("//table[@class='o_list_view table table-condensed table-striped o_list_view_ungrouped']"));
+        for(WebElement product: allProducts){
+            Assert.assertTrue(product.isDisplayed());
+        }
 
     }
 
